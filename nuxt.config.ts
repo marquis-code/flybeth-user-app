5b3976759b16072066d00cdba8f09b9d0be71036
Marquis <abahmarquis@gmail.com>
2026-05-25 03:15:41 +0100

    fix(deploy): use vercel.json for SPA routing instead of vercel_static preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   ssr: false,
 
-  nitro: { preset: 'vercel_static',
+  nitro: {
     prerender: {
       routes: ['/', '/404.html'],
       ignore: ['/dynamic-routes', '/api'],
